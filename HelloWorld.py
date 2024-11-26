8fc37be68593bedfdbbd7100d40e265ec57d54a8
Metus69420 <71786958+Metus69420@users.noreply.github.com>
2024-11-26 23:00:07 +0900

    More stuffs

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,63 @@
+# An empty loop
+a = 'geeksforgeeks'
+i = 0
+
+while i < len(a):
+    i += 1
+    pass
+
+print('Value of i :', i)
+
+""""calc = lambda num: "Even number" if num % 2 == 0 else "Odd number"
+
+print(calc(20))
+
+dic = {"a": "b", "c": "d"}
+print(dic.values())
+print(10/2)
+
+
+bool = bool()
+print(bool)
+"python"
+
+import sys
+a_list = []
+a_tuple = ()
+a_list = ["Geeks", "For", "Geeks"]
+print(a_list.clear())
+
+
+dic = {"a": "b", "c": "d"}
+dic["a"] = "ab"
+print(dic)
+tuple = 1, 2
+print(tuple)
+tuple[1] = 2
+# 슬라이싱
+string_list = ["a", "b", "c", "d"]
+number_list = [1, 2, 3, 4]
+
+# 인덱싱
+print(string_list[0]) # a
+print(number_list[2]) # 3
+
+print(string_list[2:]) # ['a', 'b']
+print(string_list[0:2] + string_list[2:4])
+print(number_list[0:3]) # [2, 3]
+#print(number_list.index(777))
+nl = []
+nl.append(0)
+print(nl)
+
+
+print(~6)
+a=1
+b=3.14
+print(a+b)
+print(b:0.1f)
+b = "a" in "applea"
+print(b)
 print("hello world?"); print("Nah")
 a = 1.23
 print(type(a))
@@ -7,7 +67,7 @@ print(f"Hi, {name}")
 a, b = 1, "a"
 print(a, b)
 
-"""
+
 
 numbers2 = [10, 20, 30, 40, 50]
 sum = 0
@@ -60,4 +120,35 @@ for key, value in beverages.items():
 user_choice = input()
 if user_choice in beverages.keys():
     consume = input("돈을 넣어주세요 ex)2000")
-    beverages.values("식혜")"""""
+    beverages.values("식혜")""
+    beverages = {
+    "사이다": 1700,
+    "콜라": 1900,
+    "식혜": 2500,
+    "솔의눈": 3000
+}
+for key, value in beverages.items():
+    print(key)
+
+user_choice = input("음료를 선택해주세요")
+
+coin = input("금액을 입력해주세요")
+print(coin)
+print(type(coin))
+
+coin = int(coin)
+print(coin)
+print(type(coin))
+print(beverages['콜라'])
+print(beverages['사이다'])
+print(beverages['식혜'])
+
+print(beverages[user_choice])
+
+if coin < beverages[user_choice]:
+    # 실행 종료하기
+    print("돈이 부족합니다")
+else:
+    remain = coin - beverages[user_choice]
+    print(remain)
+# 돈이 충분할 경우"""
